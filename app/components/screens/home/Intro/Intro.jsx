97c97df96bda1afec02d27f81d806ba0ecffb533
javlon1 <muhamedmijotot@gmail.com>
2024-06-08 13:56:09 +0500

    eqw

--- app/components/screens/home/Intro/Intro.jsx
+++ app/components/screens/home/Intro/Intro.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from './Intro.module.scss'
 import { Context } from '@/app/components/ui/Context/Context';
-import { EffectFade, Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -116,12 +116,9 @@ const Intro = () => {
 
             <div className={styles['swiper-container']}>
                 <Swiper
-                    modules={[EffectFade, Navigation, Pagination, Scrollbar, A11y]}
+                    modules={[Navigation, Pagination, Scrollbar, A11y]}
                     spaceBetween={10}
                     slidesPerView={1}
-                    effect={'fade'}
-                    fadeEffect={{ crossFade: true }}
-                    speed={2000}
                     navigation={{
                         nextEl: `.${styles['swiper-button-next-custom']}`,
                         prevEl: `.${styles['swiper-button-prev-custom']}`
