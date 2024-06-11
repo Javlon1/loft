aac345f62d32e27ea265adbea03a4f2f30beac16
javlon1 <muhamedmijotot@gmail.com>
2024-06-11 16:42:40 +0500

    seo

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -19,12 +19,12 @@ const Contact = () => {
                 <meta name="keywords" content="" /> // ключевые слова, страницы
                 <meta name="image_src" content="" /> // URL для img
 
-                <meta property="og:title" content="asa.loft | contatc" /> // Название страницы
+                <meta property="og:title" content="asa.loft | Contatc" /> // Название страницы
                 <meta property="og:description" content="" /> // Описание страницы
                 <meta property="og:image" content="" /> // URL для img: https://example.com/image.jpg
                 <meta property="og:url" content="https://loft-beryl.vercel.app/" /> // оснавное URL: https://example.com/page-url
                 <meta property="og:type" content="website" />
-                <meta property="og:site_name" content="" /> // Название сайта
+                <meta property="og:site_name" content="ASA.Loft" /> // Название сайта
                 <meta property="og:locale" content="ru_RU" />
                 <link rel="canonical" href="https://loft-beryl.vercel.app/" />
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -26,12 +26,12 @@ const Index = () => {
                 <meta name="keywords" content="" /> // ключевые слова, страницы
                 <meta name="image_src" content="" /> // URL для img
 
-                <meta property="og:title" content="Home" /> // Название страницы
+                <meta property="og:title" content="ASA.Loft | Main" /> // Название страницы
                 <meta property="og:description" content="" /> // Описание страницы
                 <meta property="og:image" content="" /> // URL для img: https://example.com/image.jpg
                 <meta property="og:url" content="https://loft-beryl.vercel.app/" /> // оснавное URL: https://example.com/page-url
                 <meta property="og:type" content="website" />
-                <meta property="og:site_name" content="" /> // Название сайта
+                <meta property="og:site_name" content="ASA.Loft" /> // Название сайта
                 <meta property="og:locale" content="ru_RU" />
                 <link rel="canonical" href="https://loft-beryl.vercel.app/" />
 
