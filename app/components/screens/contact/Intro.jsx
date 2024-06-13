5c16a1d472f4259372cdb63055f07f44ccd890e4
javlon1 <muhamedmijotot@gmail.com>
2024-06-13 21:56:31 +0500

    contact

--- app/components/screens/contact/Intro.jsx
+++ app/components/screens/contact/Intro.jsx
@@ -18,7 +18,7 @@ const Intro = () => {
         {
             id: 1,
             titel: "Адрес",
-            name: "г. Ташкент, ул. Паркент, 283",
+            name: "Andijan, Andijan region",
             icon: "fa-solid fa-location-dot"
         },
         {
@@ -36,7 +36,7 @@ const Intro = () => {
         {
             id: 4,
             titel: "Режим работы",
-            name: "Ежедневно 09:00 - 19:00",
+            name: "Ежедневно 09:00 - 18:00",
             icon: "fa-regular fa-clock"
         },
     ]
@@ -72,7 +72,7 @@ const Intro = () => {
                         </div>
 
                         <div className={styles.intro__item__list__map}>
-                            <iframe src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4876.99878707187!2d72.3288522771734!3d40.739349435939225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bced2ca7c5470d%3A0x94dd6d4ba9d395e1!2z0YPQu9C40YbQsCDQnNGD0YHRgtCw0LrQuNC70LvQuNC6IDIsINCQ0L3QtNC40LbQsNC9LCDQo9C30LHQtdC60LjRgdGC0LDQvQ!5e1!3m2!1sru!2s!4v1689419610812!5m2!1sru!2s`} width="100%" height="100%" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12080.306599303784!2d72.3010183!3d40.8043094!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bc950039f0707f%3A0xb4a115f4f00c297e!2sAsaloft.%20Loft%20Mebell!5e0!3m2!1sru!2s!4v1718297492203!5m2!1sru!2s" width="100%" height="100%" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </div>
                 </div>
