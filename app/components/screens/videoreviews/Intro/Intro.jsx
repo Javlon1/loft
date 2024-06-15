bbf71f227c55d61b8e76c6a952150ebeecb655ad
javlon1 <muhamedmijotot@gmail.com>
2024-06-15 15:38:13 +0600

    video-reviews

---
+++ app/components/screens/videoreviews/Intro/Intro.jsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from './Intro.module.scss'
+import { Context } from '@/app/components/ui/Context/Context';
+import MyContainer from '@/app/components/ui/MyContainer/MyContainer'
+import Ellipse from '@/app/components/ui/Ellipse/Ellipse';
+import video from '../../../../../public/img/slayd2.png'
+
+const Intro = () => {
+    const { lan } = React.useContext(Context);
+    const [title] = React.useState({ en: 'Video reviews', ru: 'Видео обзоры', uz: 'Video sharhlar' });
+
+    const [data, setData] = React.useState([
+        {
+            id: 1,
+            img: video,
+            title: "Item One",
+        },
+        {
+            id: 2,
+            img: video,
+            title: "Item Two",
+        },
+        {
+            id: 3,
+            img: video,
+            title: "Item Three",
+        },
+        {
+            id: 4,
+            img: video,
+            title: "Item Four",
+        },
+        {
+            id: 5,
+            img: video,
+            title: "Item Five",
+        }
+    ]);
+
+
+    return (
+        <section className={styles.intro}>
+            <MyContainer>
+                <div className={styles.intro__item}>
+                    <Ellipse />
+                    <h1 className={styles.intro__item__title}>{title[lan]}</h1>
+                    <div className={styles.intro__item__list}>
+                        {
+                            data?.map((item) => (
+                                <div key={item.id} className={styles.intro__item__list__item}>
+                                    <Image
+                                        src={item.img}
+                                        alt='user'
+                                    />
+                                    <p>{item.title}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </MyContainer>
+        </section>
+    )
+}
+
+export default Intro;
---
+++ pages/video-reviews.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import Head from 'next/head'
+import { Context } from '@/app/components/ui/Context/Context';
+import Intro from '@/app/components/screens/videoreviews/Intro/Intro';
+
+const VideoReviews = () => {
+    const { lan } = React.useContext(Context);
+    const [title] = React.useState({
+        en: 'Video-reviews',
+        ru: 'Видео-обзоры',
+        uz: 'Video-sharhlar'
+    });
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="description" content="" /> // Описание страницы
+                <meta name="keywords" content="" /> // ключевые слова, страницы
+                <meta name="image_src" content="" /> // URL для img
+
+                <meta property="og:title" content="ASA.Loft | Video-Reviews" /> // Название страницы
+                <meta property="og:description" content="" /> // Описание страницы
+                <meta property="og:image" content="" /> // URL для img: https://example.com/image.jpg
+                <meta property="og:url" content="https://loft-beryl.vercel.app/" /> // оснавное URL: https://example.com/page-url
+                <meta property="og:type" content="website" />
+                <meta property="og:site_name" content="ASA.Loft" /> // Название сайта
+                <meta property="og:locale" content="ru_RU" />
+                <link rel="canonical" href="https://loft-beryl.vercel.app/" />
+
+                <title>ASA.Loft | {title[lan]}</title>
+            </Head>
+
+            <Intro />
+        </>
+    )
+}
+
+export default VideoReviews;
