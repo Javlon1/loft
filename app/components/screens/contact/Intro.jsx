50c5b23e024c4018cf73fe399179dd677cb482d3
javlon1 <muhamedmijotot@gmail.com>
2024-06-28 21:42:37 +0500

    test v-8

--- app/components/screens/contact/Intro.jsx
+++ app/components/screens/contact/Intro.jsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import Link from 'next/link'
-import Image from 'next/image'
 import styles from './Intro.module.scss'
 import { Context } from '@/app/components/ui/Context/Context';
 import MyContainer from '@/app/components/ui/MyContainer/MyContainer'
